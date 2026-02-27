4fe2dae233b839b278d834924c278886a97b7e40
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2026-02-27 16:32:29 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+#Copyright @ISmartCoder
+#Updates Channel @abirxdhackz
+API_ID = YOUR_API_ID
+API_HASH = 'YOUR_API_HASH''
+BOT_TOKEN = 'YOUR_BOT_TOKEN'
+UPDATE_CHANNEL_URL = 't.me/abirxdhackz'
+COMMAND_PREFIXES = ['/', '!', '.', ',', '$', '#']
+
+OWNER_ID = 7666341631
+ADMIN_ID = 7666341631
