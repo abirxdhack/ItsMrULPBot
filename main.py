12fd27ca191e79160fea5611d89edccec5b53b32
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2026-02-27 16:30:47 +0600

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from app import app
-from utils import LOGGER
-from core import start_message
-from core.mongo import MONGO_CLIENT 
-
-if __name__ == "__main__":
-    LOGGER.info("Bot Successfully Started! ")
-    app.run()
